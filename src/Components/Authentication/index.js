5a1e683b5cb7cd772d77307e9323fe93bad0bed2
Ian Kaliszewski <ikalisz@yahoo.com>
2020-09-27 17:47:29 -0600

    initial cleanup

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import styled from 'styled-components'
 
 function Auth() {
   return (
-    
+    <SContainer>
+      
+    </SContainer>
   )
 }
 
 export default Auth
+
+const SContainer = styled.div`
+  display: flex;
+`
