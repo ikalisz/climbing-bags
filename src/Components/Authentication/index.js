5de69cf2466c0c849ddcbfed316e9a1e8ad406ef
Ian Kaliszewski <ikalisz@yahoo.com>
2020-10-16 22:21:38 -0600

    started login features

--- src/Components/Authentication/index.js
+++ src/Components/Authentication/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, {
+  useState,
+} from 'react'
 import styled from 'styled-components'
 
 function Auth() {
+  const [
+    username,
+    setUsername,
+  ] = useState('')
+  const [
+    password,
+    setPassword,
+  ] = useState('')
+  const login = () => {
+    const user = {
+      username,
+      password,
+    }
+  }
+
   return (
     <SContainer>
       
@@ -13,4 +30,7 @@ export default Auth
 
 const SContainer = styled.div`
   display: flex;
+`
+
+const SForm = styled.form`
 `
